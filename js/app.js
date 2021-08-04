df2d56d6411b1ec7916690e3fc47b4d81d3babf3
mabedalazim1 mabedalazim1 <mabedalazim1@gmail.com>
2021-08-04 22:05:09 +0200

    New Changes

---
+++ js/app.js
@@ -0,0 +1,16 @@
+// Selecte Landing Page Elmentes
+let landingPage = document.querySelector(".landing-page");
+
+//Get Array of Imgs
+let imgName = "../imgs/comp";
+let imgExt = ".jpg";
+let imgArray = [];
+for (i = 1; i <= 5; i++) {
+  imgArray.push(`${imgName}${i}${imgExt}`);
+}
+// Chang Background img Url
+landingPage.style.backgroundImage = `url("${imgArray[0]}")`;
+setInterval(() => {
+  let ranadomNomber = Math.floor(Math.random() * imgArray.length);
+  landingPage.style.backgroundImage = `url("${imgArray[ranadomNomber]}")`;
+}, 10000);
